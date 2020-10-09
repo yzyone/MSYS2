1dafacc2d02939345ec021b80759bb47e100fbdd
yzymibook <yzyzero@gmail.com>
2020-10-09 13:41:07 +0800

    Create hellodll1

---
+++ hellodll1/lib.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"lib.h"
+
+int foo(char*str)
+{
+        printf("%s\n", str);
+        return 0;
+}
---
+++ hellodll1/main.c
@@ -0,0 +1,6 @@
+#include"lib.h"
+int main(void)
+{
+        foo("hello world, dll from MSYS");
+        return 0;
+}
